3817b68283372afe94d83a44912b1aa4b4a70022
JacksonBurns <JacksonBurns@users.noreply.github.com>
2020-01-12 16:50:05 -0500

    pre-pull fully functional

--- ParseXML_test.py
+++ ParseXML_test.py
@@ -2,5 +2,7 @@ from os import chdir
 import Crow
 chdir(r"C:\Users\jwb1j\OneDrive\Documents\GitHub\Crow")
 root = Crow.ParseXML.ParseXML('SampleData.xml')
-for child in root:  
-    print(child)
+#for child in root:  
+#    print(child)
+#print(root[4][2][1][4].text)
+print(root[4][2])
--- PrePull.py
+++ PrePull.py
@@ -1,4 +1,5 @@
 import Crow as C
+import matplotlib.pyplot as plot
 class PrePull(C.tk.Frame):
     def __init__(self,name):
         #set up as tab
@@ -6,7 +7,7 @@ class PrePull(C.tk.Frame):
 
         #lists to place collected data and files to get it from
         self.datafiles = []
-        self.datalist = []
+        self.datadict = {}
         self.FileDisplay = C.tk.StringVar()
         self.FileDisplay = ""
         #define pre-pull callback function
@@ -15,8 +16,21 @@ class PrePull(C.tk.Frame):
             if len(self.datafiles)==0:
                 C.messagebox.showerror("Error SCIENCE FICTION REFERENCE","No data files selected!")
             else:
+                #iterate through each and pull relevant data
+                self.datadict = {}
                 for file in self.datafiles:
-                    print(file)
+                    temp = C.ParseXML.ParseXML(file)
+                    temp = temp[4][2]
+                    for peak in temp[1:]:
+                        rettime = round(float(peak[4].text),2)
+                        if (rettime in self.datadict):
+                            self.datadict[rettime]+=1
+                        else:
+                            self.datadict[rettime]=1
+                #display the histogram
+                plot.hist(list(self.datadict.values()))
+                plot.show()
+                #self.datadict.keys()
 
         #define select data callback function
         def selectdatacallback():

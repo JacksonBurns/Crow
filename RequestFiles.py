faf16fba81ae439182befd018c817ef0f1c3577b
JacksonBurns <JacksonBurns@users.noreply.github.com>
2020-01-12 00:50:56 -0500

    request files slightly more efficient

--- RequestFiles.py
+++ RequestFiles.py
@@ -1,6 +1,5 @@
-import tkinter
-from tkinter import filedialog
+from tkinter import filedialog, Tk
 def RequestFiles(file_desc,file_ext):
-    root = tkinter.Tk()
+    root = Tk()
     root.filename =  filedialog.askopenfilenames(initialdir = r"C:\Users\jwb1j\OneDrive\Desktop\hte input data",title = "Select files",filetypes = [(file_desc,file_ext)])
     return (root.filename)

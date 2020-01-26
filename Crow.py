5dcc9b8f8530088402ab181ba0eff1cbf0dc82d3
jackson <jwb1.jwb1@gmail.com>
2020-01-26 17:37:02 -0500

    'done'

--- Crow.py
+++ Crow.py
@@ -6,7 +6,7 @@ Created on Sat Jan 18 23:45:07 2020
 @author: jackson
 """
 #parent gui for all Crow family apps
-from PIL import Image, ImageTk
+#from PIL import Image, ImageTk
 import tkinter as tk
 import Crow_GC
 
@@ -51,12 +51,12 @@ def startrek3():
     SFCMSButton = tk.Button(spock, text ="SFC/MS", command = Crow_SFCMScallback, state=tk.DISABLED)
     SFCMSButton.place(x=10,y=70)
     
-    load = Image.open("Crow_logo.png")
-    load = load.resize((160,160), Image.ANTIALIAS)
-    render = ImageTk.PhotoImage(load)
-    img = tk.Label(image=render)
-    img.image = render
-    img.place(x=90,y=-15)
+#    load = Image.open("Crow_logo.png")
+#    load = load.resize((160,160), Image.ANTIALIAS)
+#    render = ImageTk.PhotoImage(load)
+#    img = tk.Label(image=render)
+#    img.image = render
+#    img.place(x=90,y=-15)
     
     spock.mainloop()
     

c455c905d9e7e2a9a0a6acde9cced446e80c9c2b
JacksonBurns <JacksonBurns@users.noreply.github.com>
2020-02-06 13:04:13 -0500

    fixed env file, commented out shebang for windows

--- Crow.py
+++ Crow.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python3.6
-# -*- coding: utf-8 -*-
+'''#!/usr/bin/python3.6
+# -*- coding: utf-8 -*-'''
 """
 Created on Sat Jan 18 23:45:07 2020
 
 @author: jackson
 """
 #parent gui for all Crow family apps
-#from PIL import Image, ImageTk
+from PIL import Image, ImageTk
 import tkinter as tk
 import Crow_GC
 
@@ -51,13 +51,12 @@ def startrek3():
     SFCMSButton = tk.Button(spock, text ="SFC/MS", command = Crow_SFCMScallback, state=tk.DISABLED)
     SFCMSButton.place(x=10,y=70)
     
-#    load = Image.open("Crow_logo.png")
-#    load = load.resize((160,160), Image.ANTIALIAS)
-#    render = ImageTk.PhotoImage(load)
-#    img = tk.Label(image=render)
-#    img.image = render
-#    img.place(x=90,y=-15)
-    
+    load = Image.open("Crow_logo.png")
+    load = load.resize((160,160), Image.ANTIALIAS)
+    render = ImageTk.PhotoImage(load)
+    img = tk.Label(image=render)
+    img.image = render
+    img.place(x=90,y=-15)
     spock.mainloop()
     
 def startrek2(spock):

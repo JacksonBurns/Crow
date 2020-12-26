e622581fd1cf3bf3657c90b2a858b936e284dd40
JacksonBurns <JacksonBurns@users.noreply.github.com>
2020-12-26 17:08:01 -0500

    publish to pypi

---
+++ setup.py
@@ -0,0 +1,25 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+cwd = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (cwd / "README.md").read_text()
+
+
+setup(
+    name="CrowHTE",
+    version="1.2.0rc1",
+    description="Python GUI to enable High Throughput Experimentation.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JacksonBurns/Crow",
+    author="Jackson Burns",
+    license="GNU GPLv3",
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+    packages=["Crow"],
+    include_package_data=True
+)
